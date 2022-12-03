73483eec1dd6230913fe6e9f9adba5ca950944d9
Abram <israelvictory87@gmail.com>
2022-12-03 11:00:38 +0100

    Created functions for ledger system

--- ledger/api.py
+++ ledger/api.py
@@ -13,3 +13,33 @@ async def ledger_home() -> dict:
         dict: version and description
     """
     return {"v1": "Welcome to Ledger Fintech!"}
+
+
+@app.post("/deposit/")
+async def deposit_money() -> dict:
+    return {}
+
+
+@app.post("/withdraw/")
+async def withdraw_money() -> dict:
+    return {}
+
+
+@app.post("/transfer/wallet-to-wallet/{wallet_from_id}/{wallet_to_id}/")
+async def wallet_to_wallet_transfer(wallet_from_id: int, wallet_to_id: int) -> dict:
+    return {}
+
+
+@app.post("/transfer/wallet-to-user/{wallet_id}/{user_id}")
+async def wallet_to_user_transfer(user_id: int) -> dict:
+    return {}
+
+
+@app.get("/balance/")
+async def total_wallet_balance() -> dict:
+    return {}
+
+
+@app.get("/balance/wallet/{wallet_id}")
+async def wallet_balance(wallet_id: int) -> dict:
+    return {}
