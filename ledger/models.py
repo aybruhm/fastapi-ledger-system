9ce799fa04325a0c7522775ae1d4cc41aa46522f
Abram <israelvictory87@gmail.com>
2022-12-03 15:40:59 +0100

    Added created_at and updated_at columns to user and wallet model

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,8 @@
+# Stdlib Imports
+import datetime
+
 # SQLAlchemy Imports
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 # Own Imports
@@ -8,21 +11,24 @@ from config.database import Base
 
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.datetime.now)
+
     wallets = relationship("Wallet", back_populates="owner")
-    
-    
+
+
 class Wallet(Base):
     __tablename__ = "users_wallet"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     user = Column(Integer, ForeignKey("users.id"))
     title = Column(String)
     amount = Column(Integer, default=0)
-    
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.datetime.now)
