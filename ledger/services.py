450a26d69bbe108509dea144a9ca9adf62d49c37
Abram <israelvictory87@gmail.com>
2022-12-03 19:48:06 +0100

    Created get_users and create_wallet service util function

--- ledger/services.py
+++ ledger/services.py
@@ -11,18 +11,14 @@ pwd_hasher = PasswordHasher()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    # Hash user password
-    hashed_password = pwd_hasher.hash_password(user.hashed_password)
 
-    # Create user
+    hashed_password = pwd_hasher.hash_password(user.hashed_password)
     db_user = models.User(email=user.email, hashed_password=hashed_password)
 
-    # Add, commmit and refresh database
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
 
-    # Return user
     return db_user
 
 
@@ -30,5 +26,20 @@ def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
 def get_user_by_email(db: Session, user_email: str):
     return db.query(models.User).filter(models.User.email == user_email).first()
+
+
+def create_wallet(db: Session, user_id: int, wallet: schemas.WalletCreate):
+
+    db_wallet = models.Wallet(user=user_id, title=wallet.title, amount=wallet.amount)
+
+    db.add(db_wallet)
+    db.commit()
+    db.refresh(db_wallet)
+
+    return db_wallet
