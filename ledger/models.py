0687036190b83d86c44ecc87d5cfb6600e0fc0a6
Abram <israelvictory87@gmail.com>
2022-12-04 15:22:52 +0100

    Added backward relationship and note

--- ledger/models.py
+++ ledger/models.py
@@ -9,6 +9,20 @@ from sqlalchemy.orm import relationship
 from config.database import Base
 
 
+# NOTE:
+# -------
+# When accessing the attribute wallets in a User, as in my_user.wallets, 
+# it will have a list of Wallet SQLAlchemy models (from the users_wallet table) that 
+# have a foreign key pointing to this record in the users table.
+
+# When you access my_user.wallets, SQLAlchemy will actually go and fetch the wallets 
+# from the database in the users_wallet table and populate them here.
+
+# And when accessing the attribute owner in an Wallet, it will contain a User 
+# SQLAlchemy model from the users table. It will use the owner_id attribute/column 
+# with its foreign key to know which record to get from the users table.
+
+
 class User(Base):
     __tablename__ = "users"
 
@@ -20,7 +34,7 @@ class User(Base):
     created_at = Column(DateTime, default=datetime.datetime.now)
     updated_at = Column(DateTime, onupdate=datetime.datetime.now)
 
-    wallets = relationship("Wallet")
+    wallets = relationship("Wallet", back_populates="owner")
 
 
 class Wallet(Base):
@@ -32,3 +46,6 @@ class Wallet(Base):
     amount = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.datetime.now)
     updated_at = Column(DateTime, onupdate=datetime.datetime.now)
+    
+    
+    owner = relationship("User", back_populates="wallets")
