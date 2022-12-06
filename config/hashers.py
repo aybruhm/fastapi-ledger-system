9fa6e4757774ae5ec8fde8991f357b1d073fb049
Abram <israelvictory87@gmail.com>
2022-12-06 21:29:21 +0100

    Refactor password hashers

--- config/hashers.py
+++ config/hashers.py
@@ -1,78 +1,38 @@
-# Stdlib Imports
-import os
-import hashlib
-
-# Own Imports
-from ledger.models import User
+# Third Party Imports
+from passlib.context import CryptContext
 
 
 class PasswordHasher:
     """
     Responsible for the following:
 
-    - generating a salt
-    - hashing the password with the generated salt
-    - check hashed password
+    - hashing password
+    - check/verify hashed password
     """
 
-    def __init__(self, salt_length: int = 38, iterations: int = 10000) -> None:
-        self.salt_length = salt_length
-        self.iterations = iterations
-
-    @property
-    def generate_salt(self):
-        """
-        This method generates a random string of
-        characters of length self.salt_length
-
-        :return: A random string of bytes.
-        """
-        return os.urandom(self.salt_length)
+    password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def hash_password(self, password: str) -> str:
         """
-        This method takes a password,
-        hashes it with a salt, and returns the hashed password.
+        This method takes a password as a string and returns a
+        hashed password as a string.
 
         :param password: The password to hash
         :type password: str
-
-        :return: The key is being returned.
+        :return: The hashed password.
         """
-        key = hashlib.pbkdf2_hmac(
-            hash_name="sha256",
-            password=password.encode("utf-8"),
-            salt=self.generate_salt,
-            iterations=self.iterations,
-        )
-        return key
+        return self.password_context.hash(password)
 
-    def check_password(self, password: str, user: User) -> bool:
+    def check_password(self, password: str, hashed_password: str) -> bool:
         """
-        This method take the password that the user entered,
-        compare it to the hashed_password stored in the user table,
-        and returns True if the password atches - otherwise False.
+        This method checks if the given password matches the hashed_password.
 
-        :param password: The password to check
+        :param password: The password to be checked
         :type password: str
-        
-        :param user: User
-        :type user: User
 
-        :return: A boolean value.
+        :param user: The hashed password that we're checking the password for
+        :type hashed_password: str
+
+        :return bool: The password context is being returned.
         """
-
-
-        password_salt = user.password["salt"]
-        password_key = user.password["key"]
-
-        hashed_password = hashlib.pbkdf2_hmac(
-            hash_name="sha256",
-            password=password.encode("utf-8"),
-            salt=password_salt,
-            iterations=self.iterations,
-        )
-
-        if hashed_password == password_key:
-            return True
-        return False
+        return self.password_context.verify(password, hashed_password)
