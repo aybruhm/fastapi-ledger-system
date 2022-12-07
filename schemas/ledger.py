6dbb934d02be1c84b47585de0116a31adc52df39
Abram <israelvictory87@gmail.com>
2022-12-07 23:54:58 +0100

    Feat: added field-level validation

--- schemas/ledger.py
+++ schemas/ledger.py
@@ -1,5 +1,15 @@
 # Pydantic Imports
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
+# FastAPI & SQLAlchemy Imports
+from fastapi import HTTPException, Depends
+from sqlalchemy.orm import Session
+
+# Core Imports
+from config.database import SessionLocal
+
+# Models Imports
+from models.ledger import Wallet as UserWallet
 
 
 class WalletBase(BaseModel):
@@ -9,6 +19,32 @@ class WalletBase(BaseModel):
 
 class WalletCreate(WalletBase):
     title: str
+
+    @validator("user", pre=True)
+    def ensure_user_can_only_have_one_wallet(cls, value: int):
+        """
+        Validation to ensure that the user can only have 10 wallets,
+        otherwise; raise an error. `@validator("user", pre=True)` means
+        that it will be called before the `user` field is validated
+
+        :param cls: The class of the model that is being validated
+
+        :param value: The value of the field being validated
+        :type value: int
+
+        :return: The value of the user_wallet_counts
+        """
+        db: Session = SessionLocal()
+        user_wallet_counts = (
+            db.query(UserWallet)
+            .join(UserWallet.owner)
+            .filter(UserWallet.user == value)
+            .count()
+        )
+
+        if user_wallet_counts == 10:
+            raise HTTPException(400, {"message": "Can only have ten wallet."})
+        return value
 
 
 class WalletDeposit(WalletBase):
