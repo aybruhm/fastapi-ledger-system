2daa2f7b6edd0f788cfac9263b88adfa19bc51a3
Abram <israelvictory87@gmail.com>
2022-12-07 08:19:52 +0100

    Update database

--- config/database.py
+++ config/database.py
@@ -1,11 +1,14 @@
+# Stdlib Imports
+import os
+
 # SQLAlchemy Imports
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ledger.sqlite"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./ledger.sqlite"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 DB_ENGINE = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
