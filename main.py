15965defe2e0491f95ec10fc608f02de61a3347a
Abram <israelvictory87@gmail.com>
2022-12-07 08:26:44 +0100

    Created startup and disconnect event for database

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # Uvicorn Imports
 import uvicorn
 
+# Own Imports
+from ledger.api import app
+from config.database import db_connect
+
+
+@app.on_event("startup")
+async def startup():
+    await db_connect.connect()
+
+
+@app.on_event("shutdown")
+async def disconnect():
+    await db_connect.disconnect()
+
 
 if __name__ == "__main__":
     uvicorn.run("ledger.api:app", host="0.0.0.0", reload=True)
