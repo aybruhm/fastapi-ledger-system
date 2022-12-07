a3c503395d32697270c61c7af51fe7781fe608ce
Abram <israelvictory87@gmail.com>
2022-12-07 21:09:26 +0100

    Created models module

---
+++ models/ledger.py
@@ -0,0 +1,22 @@
+# Stdlib Imports
+import datetime
+
+# SQLAlchemy Imports
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+# Own Imports
+from config.database import Base
+
+
+class Wallet(Base):
+    __tablename__ = "users_wallet"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user = Column(Integer, ForeignKey("users.id"))
+    title = Column(String)
+    amount = Column(Integer, default=0)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.datetime.now)
+
+    owner = relationship("User", back_populates="wallets")
---
+++ models/user.py
@@ -0,0 +1,25 @@
+# Stdlib Imports
+import datetime
+
+# SQLAlchemy Imports
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+# Own Imports
+from config.database import Base
+
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.datetime.now)
+
+    wallets = relationship("Wallet", back_populates="owner")
