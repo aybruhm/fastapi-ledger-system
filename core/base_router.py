fd86820e1150ae6590ff45c6918c86588bf470ad
Abram <israelvictory87@gmail.com>
2022-12-07 21:21:14 +0100

    Created core module

---
+++ core/base_router.py
@@ -0,0 +1,9 @@
+# FastAPI Imports
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    title="Ledger System",
+    description="A fintech backend ledger system built with FastAPI.",
+    version=1.0,
+)
---
+++ core/constants.py
@@ -0,0 +1,10 @@
+# Own Imports
+from config.database import SessionLocal
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
