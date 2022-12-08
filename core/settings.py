6f9c43b833956e23289e8decfea4f26cb6e0089f
Abram <israelvictory87@gmail.com>
2022-12-08 20:57:06 +0100

    Minor refactor: added project settings

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,11 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = config("JWT_SECRET")
     JWT_ALGORITHM: str = config("JWT_ALGORITHM")
     TOKEN_LIFETIME: int = 30
+    
+    TITLE = "Ledger System"
+    DESCRIPTION = "A fintech backend ledger system built with FastAPI."
+    CONTACT = "israelvictory87@gmail.com"
+    LICENSE = "CC0 1.0 Universal"
 
 
 ledger_settings = Settings()
