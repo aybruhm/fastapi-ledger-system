bac1d54d8c1f7a1404525db8805d6da8b20cc477
Abram <israelvictory87@gmail.com>
2022-12-21 19:13:09 +0100

    Refactor: update users/api

--- users/api.py
+++ users/api.py
@@ -1,49 +1,36 @@
 # FastAPI Imports
 from fastapi import APIRouter, Depends, HTTPException
 
-# SQLAlchemy Imports
-from sqlalchemy.orm import Session
-
 # Core Imports
-from core.constants import get_db, get_current_user
-
-# Services Imports
-from users.services import get_user, get_users
-
-# Auth Imports
-from auth.auth_bearer import JWTBearer
-
-# Models Imports
-from models.user import User as UserModel
-
-# Schemas Imports
 from schemas.user import User
+from orm.users import users_orm
+from auth.auth_bearer import jwt_bearer
+from core.constants import get_db, get_current_user, get_admin_user
 
 
 # Initialize jwt bearer and api router
-jwt_bearer = JWTBearer()
-router = APIRouter(dependencies=[Depends(jwt_bearer)], tags=["Users"])
+router = APIRouter(
+    dependencies=[
+        Depends(jwt_bearer),
+        Depends(get_db),
+    ]
+)
 
 
 @router.get("/users/", response_model=list[User])
 async def users_info(
-    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+    skip: int = 0, limit: int = 100, admin_user: User = Depends(get_admin_user)
 ):
-    db_users = get_users(db, skip, limit)
-    return db_users
+
+    if admin_user is None:
+        raise HTTPException(404, {"Admin user does not exist!"})
+
+    users = await users_orm.list(skip, limit)
+    return users
 
 
 @router.get("/users/me/", response_model=User)
-async def user_info(
-    user_id: int,
-    db: Session = Depends(get_db),
-    current_user: UserModel = Depends(get_current_user),
-):
+async def user_info(current_user: User = Depends(get_current_user)):
 
-    if current_user.id != user_id:
-        raise HTTPException(
-            401, {"message": "Unauthorized to perform this action!"}
-        )
-
-    db_user = get_user(db, user_id)
-    return db_user
+    user = await users_orm.get(user_id=current_user.id)
+    return user
