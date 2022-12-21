805bbc9935855ff0e488d57280985cfc00e21cea
Abram <israelvictory87@gmail.com>
2022-12-21 19:47:03 +0100

    Update: refactored login and create new user api controller

--- users/auth.py
+++ users/auth.py
@@ -1,48 +1,36 @@
 # FastAPI Imports
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, HTTPException
 
-# SQLAlchemy Imports
-from sqlalchemy.orm import Session
-
-# Core Imports
-from core.constants import get_db
-
-# Services Imports
-from users.services import get_user_by_email, create_user
-
-# Auth Imports
-from auth.auth_handler import AuthHandler
-from auth.hashers import PasswordHasher
-
-# Schemas Imports
+# Own Imports
+from orm.users import users_orm
+from users.services import create_user
+from auth.auth_handler import authentication
+from auth.hashers import pwd_hasher
 from schemas.user import User, UserCreate
 from schemas.auth import UserLoginSchema
 
 
-# Initialize api router,
-# auth handler, and password hasher
+# Initialize api router
 router = APIRouter()
-auth_handler = AuthHandler()
-pwd_hasher = PasswordHasher()
 
 
 @router.post("/users/", response_model=User)
-async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = get_user_by_email(db, user.email)
+async def create_new_user(new_user: UserCreate):
+    user = await users_orm.get_email(new_user.email)
 
-    if db_user:
-        raise HTTPException(400, {"message": "User already exists!"})
-    return create_user(db, user=user)
+    if user:
+        raise HTTPException(404, {"message": "User already exists!"})
+    return await create_user(new_user)
 
 
 @router.post("/login/")
-async def login_user(user: UserLoginSchema, db: Session = Depends(get_db)):
-    db_user = get_user_by_email(db, user.email)
+async def login_user(authenticate: UserLoginSchema):
+    user = await users_orm.get_email(authenticate.email)
 
-    if db_user:
-        user_token = auth_handler.sign_jwt(db_user.id)
+    if user:
+        user_token = authentication.sign_jwt(user.id)
 
-        if pwd_hasher.check_password(user.password, db_user.password):
+        if pwd_hasher.check_password(authenticate.password, user.password):
             return user_token
 
         raise HTTPException(401, {"message": "Password incorrect!"})
