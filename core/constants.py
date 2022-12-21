1c4134d38d102dda59123d87bf1f8bd9718e1ae2
Abram <israelvictory87@gmail.com>
2022-12-21 19:11:12 +0100

    Feat: implement get_admin_user dependency

--- core/constants.py
+++ core/constants.py
@@ -2,6 +2,7 @@
 from fastapi import Depends, HTTPException
 
 # Own Imports Imports
+from models.user import User
 from orm.users import users_orm
 from auth.auth_bearer import jwt_bearer
 from config.database import SessionLocal
@@ -19,8 +20,7 @@ def get_db():
         db.close()
 
 
-async def get_current_user(token: str = Depends(jwt_bearer)):
-
+async def get_current_user(token: str = Depends(jwt_bearer)) -> User:
     """
     This function takes a JWT token,
     and returns the user that the token belongs to.
@@ -30,6 +30,7 @@ async def get_current_user(token: str = Depends(jwt_bearer)):
 
     :return: The user object.
     """
+
     try:
         payload = jwt.decode(
             token, config("JWT_SECRET"), algorithms=[config("JWT_ALGORITHM")]
@@ -41,3 +42,20 @@ async def get_current_user(token: str = Depends(jwt_bearer)):
     if not user:
         raise HTTPException(404, {"message": "User does not exist!"})
     return user
+
+
+async def get_admin_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    """
+    This function returns an admin user based on the provided token;
+    otherwise, raise an authorized exception.
+    """
+
+    if current_user.is_admin is False:
+        raise HTTPException(401, {"Unauthorized to perform this action!"})
+
+    elif current_user is None:
+        return None
+    
+    return current_user
