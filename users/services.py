75e58206069b4f8768abda0c9889c8531444e6a0
Abram <israelvictory87@gmail.com>
2022-12-21 18:12:44 +0100

    Refactor: deleted get user/users function

--- users/services.py
+++ users/services.py
@@ -38,52 +38,6 @@ def create_user(db: Session, user: UserCreate):
     return db_user
 
 
-def get_user(db: Session, user_id: int):
-    """
-    This function gets a user from the database by ID.
-
-    :param db: Session
-    :type db: Session
-
-    :param user_id: The ID of the user to get
-    :type user_id: int
-
-    :return: The first user in the database with the id of user_id
-    """
-    return (
-        db.query(User)
-        .options(joinedload(User.wallets))
-        .filter(User.id == user_id)
-        .first()
-    )
-
-
-def get_users(db: Session, skip: int, limit: int):
-    """
-    This function gets all users from the database,
-    skipping the first `skip` users, and limiting
-    the result to `limit` users.
-
-    :param db: Session
-    :type db: Session
-
-    :param skip: The number of records to skip
-    :type skip: int
-
-    :param limit: The number of items to return
-    :type limit: int
-
-    :return: A list of users
-    """
-    return (
-        db.query(User)
-        .options(joinedload(User.wallets))
-        .offset(skip)
-        .limit(limit)
-        .all()
-    )
-
-
 def get_user_by_email(db: Session, user_email: str):
     """
     This function gets a user from the database by their email address.
@@ -94,7 +48,8 @@ def get_user_by_email(db: Session, user_email: str):
     :param user_email: The email address of the user you want to get
     :type user_email: str
 
-    :return: The first user in the database with the email address that matches the user_email
+    :return: The first user in the database with the email 
+    address that matches the user_email
     parameter.
     """
     return (
