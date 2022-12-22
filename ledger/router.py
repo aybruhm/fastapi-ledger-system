777a32cd45f32ecf49c919b612841f876ef06d3c
Abram <israelvictory87@gmail.com>
2022-12-22 12:43:37 +0100

    Feat: created ledger/router

---
+++ ledger/router.py
@@ -0,0 +1,15 @@
+# FastAPI Imports
+from fastapi import APIRouter, Depends
+
+# Own Imports
+from auth.auth_bearer import jwt_bearer
+from core.constants import get_db
+
+
+# initialize router
+router = APIRouter(
+    dependencies=[
+        Depends(jwt_bearer),
+        Depends(get_db),
+    ]
+)
