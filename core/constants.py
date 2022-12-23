da8990b628f2384628f3f3b95f7ae2e5d79a4530
Abram <israelvictory87@gmail.com>
2022-12-23 13:45:45 +0100

    Update: rollback transaction if exception occurs and added docstrings to get_db dependency

--- core/constants.py
+++ core/constants.py
@@ -13,9 +13,19 @@ from decouple import config
 
 
 def get_db():
+    """
+    This function creates a database session,
+    yield it to the get_db function, rollback the transaction
+    if there's an exception and then finally closes the session.
+
+    Yields:
+        db: scoped database session
+    """
     db = SessionLocal()
     try:
         yield db
+    except Exception:
+        db.rollback()
     finally:
         db.close()
 
@@ -57,5 +67,5 @@ async def get_admin_user(
 
     elif current_user is None:
         return None
-    
+
     return current_user
