b8d8d272cc23a64ece960d82b50c326871c7c3ae
Abram <israelvictory87@gmail.com>
2023-01-31 20:50:06 +0100

    Feat: created conftest module

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+# Stdlib Imports
+import sys
+import os
+from typing import Any
+
+# SQLAlchemy Impprts
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Own Imports
+from models.user import Base
+
+# Third Party Imports
+import pytest
+
+
+# this is to include backend dir in sys.path so that we can import from db, main.py
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.sqlite"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Use connect_args parameter only with sqlite
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="function")
+def create_tables():
+    """
+    Create a fresh database on each test case.
+    """
+    Base.metadata.drop_all(engine)
+    yield
+    Base.metadata.create_all(engine)  # Create the tables.
+
+
+@pytest.fixture(scope="function")
+def _get_test_db():
+    session = SessionTesting()
+    try:
+        yield session
+    except Exception:
+        session.rollback()
+    finally:
+        session.close()
